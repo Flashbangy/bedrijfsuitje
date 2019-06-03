c41eb2dfaba84e6c5ac2e281b766d1a30802e339
Flashbangy <44573779+Flashbangy@users.noreply.github.com>
2019-06-03 15:16:04 +0200

    Initial commit

---
+++ 404.php
@@ -0,0 +1,3 @@
+you have big gay
+
+<a href="index.php?page=home">  CLICK HERE TO RESTORE YOUR GAY</a>
---
+++ signup.php
@@ -0,0 +1,25 @@
+<form action="" method="post">
+  <div class="form-group">
+    <label for="gebruikersnaam">Gebruikersnaam</label>
+    <input type="text" class="form-control" id="gerbruikersnaam"  placeholder="Gebruikersnaam">
+    <small class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="email">Email address</label>
+    <input type="text" class="form-control" id="email" placeholder="E-mail">
+    <small  class="form-text text-muted"></small>
+  </div>
+  <div class="form-group">
+    <label for="wachtwoord">Wachtwoord</label>
+    <input type="password" class="form-control" id="wachtwoord" placeholder="Wachtwoord">
+    <div class="form-group">
+    <label for="voornaam">voornaam</label>
+    <input type="text" class="form-control" id="voornaam" name="voornaam" placeholder="Voornaam">
+  </div>
+  <div class="form-group">
+    <label for="tussenvoegsel">tussenvoegsel</label>
+    <input type="text" class="form-control" id="tussenvoegsel" placeholder="Wachtwoord">
+  </div>
+  </div>
+  <button type="submit" value="aanmelden" name="aanmelden" class="btn btn-primary">Aanmelden</button>
+</form>
